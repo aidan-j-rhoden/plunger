6da6d7c84369610509f1fa9152c83c552925f889
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2023-03-11 18:40:06 -0600

    More updates and stuff

--- weapons/bathtub/bathtub.gd
+++ weapons/bathtub/bathtub.gd
@@ -1,19 +1,15 @@
 extends StaticBody3D
 
-const HEAL_FACTOR = 2
+const HEAL_FACTOR = 3
 var user
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if user:
 		user.heal(delta * HEAL_FACTOR)
 
-
+# This area is set to the layer the player is on,
+# so we don't need to check if the body is a player
 func _on_area_3d_body_entered(body):
 	user = body
 
