9213a0a4c2936b0c860538291b98adbe4ec0781a
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2023-03-14 08:16:38 -0500

    Work on plunger launch

--- weapons/plunger/item_plunger.gd
+++ weapons/plunger/item_plunger.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+@onready var item = preload("res://weapons/plunger/plunger.tscn").instantiate()
+@onready var weapons_node = get_node("/root/World/weapons")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -7,4 +10,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	if Input.is_action_just_pressed("shoot"):
+		item.chucked = true
+		weapons_node.add_child(item)
+		item.rotation_degrees = Vector3(0, 0, 90)
+		item.global_transform = global_transform
+		item.apply_impulse(Vector3(0, 5, -25))
+		queue_free()
--- weapons/plunger/plunger.gd
+++ weapons/plunger/plunger.gd
@@ -2,6 +2,7 @@ extends RigidBody3D
 
 var type = "plunger"
 var picked = false
+var chucked = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,8 +14,13 @@ func _process(_delta):
 	pass
 
 
+func _physics_process(_delta):
+	if chucked:
+		lock_rotation = true
+
+
 func _on_area_3d_body_entered(body):
-	if not picked:
+	if not picked and not chucked:
 		picked = true
 		body.gain_weapon(type)
 		queue_free()
