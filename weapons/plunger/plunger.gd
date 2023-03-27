61a6ada99bd1bceacbd9baaa544479a9ebb07104
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2023-03-26 20:02:07 -0500

    Begin work on plunger
    Much code taken from https://www.youtube.com/watch?v=6GIrjs-Qy0U

--- weapons/plunger/plunger.gd
+++ weapons/plunger/plunger.gd
@@ -1,5 +1,12 @@
-extends RigidBody3D
+extends CharacterBody3D
 class_name Plunger
+
+enum STATE {HELD, THROWN, LANDED}
+var state: STATE = STATE.HELD
+var spin_speed = 20
+var throw_force = 10
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var type = "plunger"
 var picked = false
@@ -17,10 +24,37 @@ func _ready():
 	animation_tree[squish_param] = "reset"
 
 
-func _physics_process(_delta):
-	if chucked:
-		lock_rotation = true
-	lvel = linear_velocity.length()
+func _physics_process(delta):
+	if state == STATE.THROWN:
+		rotate_object_local(Vector3.RIGHT, deg_to_rad(spin_speed))
+
+	if state == STATE.THROWN:
+		if !is_on_floor():
+			velocity.y -= gravity * delta
+		move_and_collide(velocity * delta)
+
+
+func throw():
+	if state == STATE.HELD:
+		top_level = true
+		var direction = _get_direction()
+		transform = transform.looking_at(global_position + direction, Vector3.UP)
+		velocity = direction * throw_force
+		state = STATE.THROWN
+
+
+func _get_direction():
+	var viewport := get_viewport()
+	var camera := viewport.get_camera_3d()
+	var center: Vector2 = viewport.size/2
+	var from: Vector3 = camera.project_ray_origin(center)
+	var to: Vector3 = from + camera.project_ray_normal(center) * 1000
+	var query := PhysicsRayQueryParameters3D.create(from, to)
+	var collision = get_world_3d().direct_space_state.intersect_ray(query)
+	if collision:
+		return global_position.direction_to(collision.position)
+	else:
+		return global_position.direction_to(to)
 
 
 func squish():
@@ -40,3 +74,5 @@ func _on_plunger_cup_body_entered(body):
 		return
 	if lvel >= 10:
 		squish()
+	if body is Player:
+		body.hurt(30)
