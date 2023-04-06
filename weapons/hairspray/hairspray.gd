1127c87922b32d5c21d92aea2345ddf2cbd7c726
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2023-04-06 14:42:53 -0500

    A little work on fire here and there

--- weapons/hairspray/hairspray.gd
+++ weapons/hairspray/hairspray.gd
@@ -3,11 +3,20 @@ extends RigidBody3D
 enum STATE {HELD, FIRING, LANDED}
 @export var state: STATE = STATE.LANDED
 var type = "hairspray"
+@export var fuel = 25.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if get_parent_node_3d().name == "player_weapons":
 		state = STATE.HELD
+
+
+func _process(delta):
+	if state == STATE.FIRING:
+		fuel -= delta
+		if fuel <= 0.0:
+			state = STATE.HELD
+			fire()
 
 
 func _physics_process(_delta):
@@ -16,12 +25,15 @@ func _physics_process(_delta):
 
 
 func fire():
-	if state != STATE.FIRING:
+	if state != STATE.FIRING and fuel > 0.0:
 		state = STATE.FIRING
 		$AnimationPlayer.play("spray")
 	else:
 		state = STATE.HELD
-		$AnimationPlayer.play_backwards("spray")
+		if fuel > 0.0:
+			$AnimationPlayer.play_backwards("spray")
+		else:
+			$AnimationPlayer.play("RESET")
 
 
 func _on_pickup_body_entered(body): # This area only detects the player layer
