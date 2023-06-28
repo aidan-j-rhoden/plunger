dbc73718c77768d878a55b6eee1f78e8d33104ee
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2023-06-28 13:30:45 -0500

    Got the x rotation working

--- player/PlayerInput.gd
+++ player/PlayerInput.gd
@@ -4,14 +4,14 @@ extends MultiplayerSynchronizer
 @export var jumping := false
 @export var crouching := false
 @export var firing := false
-@export var selected_weapon = 0
+#@export var selected_weapon = 0
 
 # Synchronized property.
 @export var direction := Vector2()
-@export var x_rot = 0
-@export var y_rot = 0
+@export var x_rot:float = 0.0
+@export var y_rot:float = 0.0
 
-const CAMERA_ROT_SPEED = 0.05
+const CAMERA_ROT_SPEED:float = 0.1
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -37,23 +37,23 @@ func fire():
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		y_rot = -event.relative.x * CAMERA_ROT_SPEED
-		x_rot = -event.relative.y * CAMERA_ROT_SPEED
+		y_rot = float(-event.relative.x) * CAMERA_ROT_SPEED
+		x_rot = float(-event.relative.y) * CAMERA_ROT_SPEED
 
 
 func _process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	direction = Input.get_vector("left", "right", "forward", "back")
-	if Input.is_action_just_pressed("next_weapon"):
-		selected_weapon += 1
-		if selected_weapon > 2:
-			selected_weapon = 0
-		$"..".update_weapons_hud()
-	if Input.is_action_just_pressed("prev_weapon"):
-		selected_weapon -= 1
-		if selected_weapon < 0:
-			selected_weapon = 2
-		$"..".update_weapons_hud()
+#	if Input.is_action_just_pressed("next_weapon"):
+#		selected_weapon += 1
+#		if selected_weapon > 2:
+#			selected_weapon = 0
+#		$"..".update_weapons_hud()
+#	if Input.is_action_just_pressed("prev_weapon"):
+#		selected_weapon -= 1
+#		if selected_weapon < 0:
+#			selected_weapon = 2
+#		$"..".update_weapons_hud()
 	if Input.is_action_just_pressed("jump"):
 		jump.rpc()
 	if Input.is_action_just_pressed("crouch"):
--- player/player.gd
+++ player/player.gd
@@ -1,17 +1,17 @@
 extends CharacterBody3D
 class_name Player
 
+@onready var input = $PlayerInput
 @onready var camera = $Camera3D
 
 const SPEED = 8.5
 const JUMP_VELOCITY = 11
-const CAMERA_ROT_SPEED = 15
 
 var gravity = 30
 
 ## An inventory of all me weapons.  The keys must be in the same order that $HUD/weapons/ is organized, or tons of things will die horrible deaths.
 @export var weapons = {"plungers": 0, "hairsprays": 0, "toilet_papers": 0}
-@onready var selected_weapon = $PlayerInput.selected_weapon
+#@onready var selected_weapon = $PlayerInput.selected_weapon
 
 #Hud stuff
 @onready var plunger_count = $HUD/weapons/plungers/count
@@ -33,7 +33,7 @@ func _enter_tree(): # Set the authority of the PlayerInput node to this node's n
 
 func _ready():
 	$player_name.text = Globals.player_names[$PlayerInput.get_multiplayer_authority()] # Set the 3d text label
-	update_weapons_hud()
+#	update_weapons_hud()
 
 	if str(name).to_int() == multiplayer.get_unique_id(): # If this client is associated with this player, 
 		camera.current = true # use this camera,
@@ -43,46 +43,46 @@ func _ready():
 
 
 func _physics_process(delta):
-	rotate_y($PlayerInput.y_rot * delta * CAMERA_ROT_SPEED)
-	camera.rotate_x($PlayerInput.x_rot * delta * CAMERA_ROT_SPEED)
-	$PlayerInput.y_rot = 0.0
-	$PlayerInput.x_rot = 0.0
+	rotate_y(input.y_rot * delta)
+	camera.rotate_x(input.x_rot * delta)
+	input.y_rot = 0.0
+	input.x_rot = 0.0
 	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -89.9, 89.9)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if $PlayerInput.jumping and is_on_floor():
+	if input.jumping and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	$PlayerInput.jumping = false
+	input.jumping = false
 
 	# Handle crouch.  TODO: Doesn't work.
-	if $PlayerInput.crouching and is_on_floor() and not crouched:
+	if input.crouching and not crouched:
 		crouched = true
-		$PlayerInput.crouching = false
+		input.crouching = false
 		$AnimationPlayer.play("crouch")
-	elif $PlayerInput.crouching and crouched:
+	elif input.crouching and crouched:
 		crouched = false
-		$PlayerInput.crouching = false
+		input.crouching = false
 		$AnimationPlayer.play_backwards("crouch")
 
 	# Handle Fire.
-	if $PlayerInput.firing:
-		$PlayerInput.firing = false
-		var item_list = []
-		for key in weapons.keys():
-			item_list.append(key)
-		if item_list[selected_weapon] == "plungers" and weapons["plungers"] > 0:
-			$player_weapons/plunger.throw()
-			weapons["plungers"] -= 1
-			update_weapons_hud()
-		elif item_list[selected_weapon] == "hairsprays" and weapons["hairsprays"] > 0:
-			$player_weapons/hairspray.fire()
-			update_weapons_hud()
+	if input.firing:
+		input.firing = false
+#		var item_list = []
+#		for key in weapons.keys():
+#			item_list.append(key)
+#		if item_list[selected_weapon] == "plungers" and weapons["plungers"] > 0:
+#			$player_weapons/plunger.throw()
+#			weapons["plungers"] -= 1
+#			update_weapons_hud()
+#		elif item_list[selected_weapon] == "hairsprays" and weapons["hairsprays"] > 0:
+#			$player_weapons/hairspray.fire()
+#			update_weapons_hud()
 
 	# Get the input direction and handle the movement/deceleration.
-	var direction = (transform.basis * Vector3($PlayerInput.direction.x, 0, $PlayerInput.direction.y)).normalized()
+	var direction = (transform.basis * Vector3(input.direction.x, 0, input.direction.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -93,41 +93,41 @@ func _physics_process(delta):
 	move_and_slide()
 
 
-func gain_weapon(type):
-	if type == "plunger":
-		weapons["plungers"] += 1
-	elif type == "hairspray":
-		weapons["hairsprays"] += 1
-	update_weapons_hud()
+#func gain_weapon(type):
+#	if type == "plunger":
+#		weapons["plungers"] += 1
+#	elif type == "hairspray":
+#		weapons["hairsprays"] += 1
+#	update_weapons_hud()
 
 
-func update_weapons_hud():
-	plunger_count.text = str(weapons["plungers"])
-	hairspray_count.text = str(weapons["hairsprays"])
-	toilet_papers_count.text = str(weapons["toilet_papers"])
-	var item_list = []
-	for key in weapons.keys():
-		item_list.append(key)
-
-	# Reset everything
-	plunger_box.modulate = Color(0, 0, 0, 255)
-	hairsprays_box.modulate = Color(0, 0, 0, 255)
-	toilet_papers_box.modulate = Color(0, 0, 0, 255)
-	for child in $player_weapons.get_children():
-		child.visible = false
-
-	selected_weapon = $PlayerInput.selected_weapon
-
-	if item_list[selected_weapon] == "plungers":
-		plunger_box.modulate = Color(1, 1, 1, 1)
-		if weapons["plungers"] > 0:
-			$player_weapons/plunger.visible = true
-	elif item_list[selected_weapon] == "hairsprays":
-		hairsprays_box.modulate = Color(1, 1, 1, 1)
-		if weapons["hairsprays"] > 0:
-			$player_weapons/hairspray.visible = true
-	elif item_list[selected_weapon] == "toilet_papers":
-		toilet_papers_box.modulate = Color(1, 1, 1, 1)
+#func update_weapons_hud():
+#	plunger_count.text = str(weapons["plungers"])
+#	hairspray_count.text = str(weapons["hairsprays"])
+#	toilet_papers_count.text = str(weapons["toilet_papers"])
+#	var item_list = []
+#	for key in weapons.keys():
+#		item_list.append(key)
+#
+#	# Reset everything
+#	plunger_box.modulate = Color(0, 0, 0, 255)
+#	hairsprays_box.modulate = Color(0, 0, 0, 255)
+#	toilet_papers_box.modulate = Color(0, 0, 0, 255)
+#	for child in $player_weapons.get_children():
+#		child.visible = false
+#
+#	selected_weapon = $PlayerInput.selected_weapon
+#
+#	if item_list[selected_weapon] == "plungers":
+#		plunger_box.modulate = Color(1, 1, 1, 1)
+#		if weapons["plungers"] > 0:
+#			$player_weapons/plunger.visible = true
+#	elif item_list[selected_weapon] == "hairsprays":
+#		hairsprays_box.modulate = Color(1, 1, 1, 1)
+#		if weapons["hairsprays"] > 0:
+#			$player_weapons/hairspray.visible = true
+#	elif item_list[selected_weapon] == "toilet_papers":
+#		toilet_papers_box.modulate = Color(1, 1, 1, 1)
 
 
 func hurt(amount):
