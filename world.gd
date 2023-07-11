1b80d5314a2034c2ed026358c142bbd3ea31348c
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2023-07-11 14:31:29 -0500

    Even more room work

--- world.gd
+++ world.gd
@@ -6,11 +6,11 @@ func _ready():
 	multiplayer.peer_disconnected.connect(remove_player)
 
 
-func add_players(players):
-	for ID in players:
+func add_player(id):
+	if multiplayer.is_server():
 		var player = player_scene.instantiate()
-		player.name = str(ID)
-		get_node("players").add_child(player)
+		player.name = str(id)
+		get_node("map/players").add_child(player)
 
 
 func remove_player(peer_id):
